0cbfefffe0c24aed40f2e98abd34a48c78c44eb7
DylanVallejo <dylan1811vallejo@gmail.com>
2024-05-07 23:51:05 -0500

    crando interfaces se debe segmentar la interfaz si se posee dentro de ella objetos

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import './style.css'
 // import { setupCounter } from './counter.ts'
 // import './topics/01-basic-types.ts';
 // import './topics/02-object-interface.ts';
-import './topics/03-functions';
+// import './topics/03-functions';
+import './topics/04-homework-types';
+
 
 
 
